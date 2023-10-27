dd31e0d80eec4920ec10760d7e2f77d5a03c76b3
k-jaehyun <wogus146@gmail.com>
2023-10-27 21:11:56 +0900

    이거 왜 안되는거야ㅐ!!

--- Init.java
+++ Init.java
@@ -8,7 +8,9 @@ public class Init {
     private long totalPrice;
     private int orderNumber;
     private HashMap<Integer, List<Item>> dailyOrderList;
+    public ArrayList<List<Item>> waitItemList = new ArrayList<>();  //waitingNumber랑 세트
     private long dailySales;
+    private long waitingNumber;
     private int dailyOrderCount;
     Scanner scanner = new Scanner(System.in);
     private String inputString;
@@ -40,27 +42,35 @@ public class Init {
     //메뉴 초기화, menuItems(List<Item>)에 burgerMenu(HashMap<"메뉴분류", List<Item>) 입력, 그리고 firstPage로 이동
     void initMenuItems() {
         List<Item> burgerMenu = new ArrayList<>();
-        burgerMenu.add(new Item("ShackBurger", "토마토, 양상추, 쉑소스가 토핑된 치즈버거", 6900));
-        burgerMenu.add(new Item("SmokeShack", "애플 우드 칩으로 훈연한 베이컨, 매콤한 체리 페퍼에 쉐소스가 토핑된 치즈 버거", 8900));
-        burgerMenu.add(new Item("ShroomBurger", "몬스터 치즈와 체다 치즈로 속을 채우고 바삭하게 뒤겨낸 포토벨로 버섯 패티에 양상추, 토마토, 쇅소스를 올린 베지테리안 버거", 9400));
-        burgerMenu.add(new Item("Shack Stack", "슈룸 버거와 쉑버거의 맛을 한번에 즐길 수 있는 메뉴", 12400));
-        burgerMenu.add(new Item("Cheeseburger", "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거", 6900));
-        burgerMenu.add(new Item("Hamburger", "포테이토 번과 비프패티를 기본으로 신선한 양상추, 토마토 피클, 양파 토핑을 취향에 따라 선택할 수 있는 버거", 5400));
+        burgerMenu.add(new Item("ShackBurger", "토마토, 양상추, 쉑소스가 토핑된 치즈버거", 6900, "burger_1"));
+        burgerMenu.add(new Item("SmokeShack", "애플 우드 칩으로 훈연한 베이컨, 매콤한 체리 페퍼에 쉐소스가 토핑된 치즈 버거", 8900, "burger_2"));
+        burgerMenu.add(new Item("ShroomBurger", "몬스터 치즈와 체다 치즈로 속을 채우고 바삭하게 뒤겨낸 포토벨로 버섯 패티에 양상추, 토마토, 쇅소스를 올린 베지테리안 버거", 9400, "burger_3"));
+        burgerMenu.add(new Item("Shack Stack", "슈룸 버거와 쉑버거의 맛을 한번에 즐길 수 있는 메뉴", 12400, "burger_4"));
+        burgerMenu.add(new Item("Cheeseburger", "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거", 6900, "burger_5"));
+        burgerMenu.add(new Item("Hamburger", "포테이토 번과 비프패티를 기본으로 신선한 양상추, 토마토 피클, 양파 토핑을 취향에 따라 선택할 수 있는 버거", 5400, "burger_6"));
         List<Item> frozenCustardMenu = new ArrayList<>();
-        frozenCustardMenu.add(new Item("Shake", "바닐라, 초콜렛, 솔티드 카라멜, 블랙& 화이트, 스트로베리, 피넛버터, 커피", 5900));
-        frozenCustardMenu.add(new Item("Shake of the Week", "특별한 커스터드 플레이버", 6500));
-        frozenCustardMenu.add(new Item("Red Bean Shake", "신선한 커스터드와 함께 우유와 레드빈이 블렌딩 된 시즈널 쉐이크", 6500));
-        frozenCustardMenu.add(new Item("Floats", "루트 비어, 퍼플 카우, 크림시클", 5900));
-        frozenCustardMenu.add(new Item("Cups&Cones", "바닐라, 초콜렛, Flavor of the Week", 4900));
-        frozenCustardMenu.add(new Item("Concretes", "쉐이크쉑의 쫀득한 커스터드와 다양한 믹스-인의 조합", 5900));
+        frozenCustardMenu.add(new Item("Shake", "바닐라, 초콜렛, 솔티드 카라멜, 블랙& 화이트, 스트로베리, 피넛버터, 커피", 5900, "dessert_1"));
+        frozenCustardMenu.add(new Item("Shake of the Week", "특별한 커스터드 플레이버", 6500, "dessert_2"));
+        frozenCustardMenu.add(new Item("Red Bean Shake", "신선한 커스터드와 함께 우유와 레드빈이 블렌딩 된 시즈널 쉐이크", 6500, "dessert_3"));
+        frozenCustardMenu.add(new Item("Floats", "루트 비어, 퍼플 카우, 크림시클", 5900, "dessert_4"));
+        frozenCustardMenu.add(new Item("Cups&Cones", "바닐라, 초콜렛, Flavor of the Week", 4900, "dessert_5"));
+        frozenCustardMenu.add(new Item("Concretes", "쉐이크쉑의 쫀득한 커스터드와 다양한 믹스-인의 조합", 5900, "dessert_6"));
         List<Item> drinkMenu = new ArrayList<>();
-        drinkMenu.add(new Item("Shack-made Lemonade", "매장에서 직접 만드는 상큼한 레몬에이드", 3900));
-        drinkMenu.add(new Item("Fresh Brewed Ice Tea", "직접 유기농 홍차를 우려낸 아이스티", 3400));
-        drinkMenu.add(new Item("Fifty/Fifty", "레몬에이트와 아이스티의 만남", 3500));
-        drinkMenu.add(new Item("Fountain Soda", "코카콜라, 코카콜라 제로, 스프라이트, 환타 오렌지, 환타 그레이프", 2700));
-        drinkMenu.add(new Item("Abita Root Beer", "청량감 있는 독특한 미국식 무알콜 단산음료", 4400));
-        drinkMenu.add(new Item("Bottled Water", "지리산 암반대수층으로 만든 프리미엄 생수", 1000));
-        drinkMenu.add(new Item("ShackMeister Ale", "쉐이크쉑 버거를 위해 뉴욕 브루클린 브루어리에서 특별히 양조한 에일 맥주", 9800));
+        drinkMenu.add(new Item("Shack-made Lemonade", "매장에서 직접 만드는 상큼한 레몬에이드", 3900, "drink_1"));
+        drinkMenu.add(new Item("Fresh Brewed Ice Tea", "직접 유기농 홍차를 우려낸 아이스티", 3400, "drink_2"));
+        drinkMenu.add(new Item("Fifty/Fifty", "레몬에이트와 아이스티의 만남", 3500, "drink_3"));
+        drinkMenu.add(new Item("Fountain Soda", "코카콜라, 코카콜라 제로, 스프라이트, 환타 오렌지, 환타 그레이프", 2700, "drink_4"));
+        drinkMenu.add(new Item("Abita Root Beer", "청량감 있는 독특한 미국식 무알콜 단산음료", 4400, "drink_5"));
+        drinkMenu.add(new Item("Bottled Water", "지리산 암반대수층으로 만든 프리미엄 생수", 1000, "drink_6"));
+        drinkMenu.add(new Item("ShackMeister Ale", "쉐이크쉑 버거를 위해 뉴욕 브루클린 브루어리에서 특별히 양조한 에일 맥주", 9800, "drink_7"));
+        List<Item> menu4 = new ArrayList<>();
+        List<Item> menu5 = new ArrayList<>();
+        List<Item> menu6 = new ArrayList<>();
+        List<Item> menu7 = new ArrayList<>();
+        List<Item> menu8 = new ArrayList<>();
+        List<Item> menu9 = new ArrayList<>();
+        List<Item> menu10 = new ArrayList<>();
+        List<Item> menu11 = new ArrayList<>();
 
         menuItems.put("burger", burgerMenu);
         menuItems.put("frozenCustard", frozenCustardMenu);
@@ -102,7 +112,7 @@ public class Init {
                =========================================================
                
                
-                [ 1. 판매 현황  | 2. 주문 목록 | 3. 메뉴 관리 | 4. 뒤로 가기 5.대기 주문 목록]
+                [ 1. 판매 현황  | 2. 주문 목록 | 3. 메뉴 관리 | 4. 뒤로 가기 | 5. 대기주문 완료시키기]
               
               
                =========================================================
@@ -112,9 +122,7 @@ public class Init {
         System.out.print(">");
         switch (select) {
             case 5:
-                //for(int i=0;i<waitingOrderList.size();i++){
-                    //System.out.println(waitingOrderList.get(i).get(i).name);
-                //}
+                deleteWait();
             case 1:
                 System.out.println("============================");
                 System.out.println(" [ Daily Report ] ");
@@ -484,8 +492,13 @@ public class Init {
                     System.out.println("=============================================");
                     System.out.println();
                     System.out.println(" 주문해주셔서 감사합니다\n");
-                    System.out.printf(" [ 주문 번호: %d ]\n",  ++orderNumber);                     // 하루 주문 건수
+                    System.out.printf(" [ 주문 번호: %d ]\n",  ++orderNumber, ++waitingNumber);                     // 하루 주문 건수
                     dailyOrderList.put(orderNumber, new ArrayList<>(cart));
+
+                        this.waitItemList.add(cart);
+                        System.out.println(cart+"cart가 wait에 담김");
+
+                    System.out.println("대기번호: " + waitingNumber);
                     System.out.println();
                     System.out.println();
                     System.out.println("=============================================");
@@ -535,6 +548,7 @@ public class Init {
 
 
     public void addMenu() {
+        Scanner scanner = new Scanner(System.in);
         System.out.print("추가할 메뉴의 종류를 입력해 주세요");
         System.out.print(">");
         String menu = scanner.nextLine();
@@ -547,8 +561,12 @@ public class Init {
         String description = scanner.nextLine();
         System.out.print("추가할 메뉴의 가격을 입력해 주세요");
         System.out.print(">");
-        long price = scanner.nextLong();
-        menuItems.get(menu).add(0, new Item(name, description, price));
+        int price = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("추가할 메뉴의 ID를 입력해 주세요");
+        System.out.print(">");
+        String id = scanner.nextLine();
+        menuItems.get(menu).add(0, new Item(name, description, price, id));
         System.out.println("메뉴가 추가되었습니다");
         addDeletePage();
     }
@@ -579,5 +597,36 @@ public class Init {
         }
 
         addDeletePage();
+    }
+
+    //재현 (주문대기를 완료시키는 메서드)
+    public void deleteWait() {
+        System.out.println("대기주문 완료시키는 페이지\n");
+        System.out.println("[대기주문목록]");
+
+        //아이탬이름, 대기번호 출력
+        for(int i=0;i<waitItemList.size();i++) {
+            System.out.println("i: "+i);
+            for (int j = 0; j < waitItemList.get(i).size(); j++) {
+                System.out.println("j:"+j);
+                System.out.println("메뉴: " + waitItemList.get(i).get(j).name + " | 대기번호: " + i + 1);
+            }
+        }
+
+        System.out.println("대기번호를 입력해서 완료시키기: ");
+        Scanner scanner = new Scanner(System.in);
+        int inputOrderNumber = scanner.nextInt()+1;
+
+//        System.out.println(waitItemList.get(inputOrderNumber).name+"대기번호"+inputOrderNumber+"가 제거되었습니다.");
+        for (int i=0;i<waitItemList.get(inputOrderNumber).size();i++) {
+            System.out.println(waitItemList.get(inputOrderNumber).get(i).name);
+        }
+
+        waitItemList.remove(inputOrderNumber);
+        --waitingNumber;
+
+        firstPage();
+
+
     }
 }
--- Item.java
+++ Item.java
@@ -1,9 +1,11 @@
 
 public class Item extends Menu {
     long price;
+    String id;
 
-    public Item(String name, String description, long price) {
+    public Item(String name, String description, long price, String id) {
         super(name, description);
         this.price = price;
+        this.id = id;
     }
 }
